670c0bb8bcb61d449f3fe727239d5369de908eb3
PART00007 <parteel_pw2_040@masai.school>
2022-04-27 17:14:59 +0530

    sort

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQH82D1DBTHRZuDrpt4wNaMYkGxunGyLHThg&usqp=CAU",
                     name:"TALVAR",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:6,
                    
                  
                 },
@@ -12,7 +12,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvfYhxAzpeHHDfnSfb9b6AUeD_iuPSJOGvvg&usqp=CAU",
                     name:"LIFE",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:8,
                    
                  
                 },
@@ -20,7 +20,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2Az25g8mg71ajITsPzeSML3ujwlsROIEMYw&usqp=CAU",
                     name:"HERO",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:7,
                    
                    
                  
@@ -29,7 +29,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0VsSKFfY6GKN0dOhLMls7Pn96Xb-L7U12KA&usqp=CAU",
                     name:"RA-ONE",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:10,
                    
                  
                 },
@@ -37,7 +37,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzCD7C3_J0zEn6leZwCMAy4lYxOJ4BzIZQAQ&usqp=CAU",
                     name:"GORKHA",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:7,
                    
                  
                 },
@@ -45,7 +45,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRll9YkCuBYYfTDtAFMm9XAHsBllkwJQgd7Ag&usqp=CAU",
                     name:"HAIDER",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:4,
                    
                  
                 },
@@ -53,7 +53,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnVH8X4p3fk8e1lhPcx2rxffL78XxBUNJryA&usqp=CAU",
                     name:"BAD-BOYS",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:5,
                    
                  
                 },
@@ -61,7 +61,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyv2ig-FfsebVeELExvCMQwdLO-VaXtkvt2w&usqp=CAU",
                     name:"BRING HIM HOME",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:10,
                    
                  
                 },
@@ -69,7 +69,7 @@
                     imge_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq4Dq7tb4v4mhxl38SpOoHlrWu7muZuDdm-g&usqp=CAU",
                     name:"JAI-BHIM",
                     date:"11.08.2022",
-                    rating:"9.8",
+                    rating:2,
                    
                  
                 },
@@ -134,16 +134,16 @@
     document.querySelector("#SortInput").addEventListener("change",function(){
             var SortType=document.querySelector("#SortInput").value;
             if(SortType==='none'){
-                displaydata(earphonedata)
+                displaydata(moviedata)
             }else{
 
             
            filterdata.sort(function(a,b){
                
-                if(SortType=="lowtohigh"){
-                    return a.price1-b.price1;
+                if(SortType=="sort-lh"){
+                    return a.rating-b.rating;
                 }
-                return b.price1-a.price1
+                return b.rating-a.rating;
             });
         
          displaydata(filterdata)
@@ -155,17 +155,18 @@ displaydata(moviedata);
    document.querySelector("#SortInput").addEventListener("change",function(){
             var SortType=document.querySelector("#SortInput").value;
             if(SortType==='none'){
-                displaydata(earphonedata)
+                displaydata(movieedata)
             }else{
 
             
            filterdata.sort(function(a,b){
                
-                if(SortType=="A-Z"){
-                  
-                }
-            
-            });
+              
+            if(SortType=="sort-hl"){
+                return b.rating-a.rating;
+            }
+            return a.rating-b.rating;
+        });
         
          displaydata(filterdata)
           }
